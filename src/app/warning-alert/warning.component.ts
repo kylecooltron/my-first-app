8f72f1adc9b4bd71e31a43e31ecdb76989ba9cf0
Kyle Coulon <kylejcoulon@gmail.com>
2023-01-11 22:36:48 -0700

    basics

---
+++ src/app/warning-alert/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning.component.html',
+    // styles: [
+    //     `h3 {
+    //       color: darkblue;
+    //     }`
+    //   ]
+})
+export class WarningAlertComponent { }
